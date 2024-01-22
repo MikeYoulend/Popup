f4e2fd3ead963701547b06b0593548043d4bf525
MikeYoulend <mikeyoulend@gmail.com>
2024-01-22 13:43:06 +0100

    Popup

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 let popup = document.getElementById("popup");
 
-function openPopup() {}
+function openPopup() {
+	popup.classList.add("open-popup");
+}
+
+function closePopup() {
+	popup.classList.remove("open-popup");
+}
